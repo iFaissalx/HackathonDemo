6b24f83c442604b8e0b776c0725166f754ac9092
iFaissalx <61896212+iFaissalx@users.noreply.github.com>
2025-12-11 02:55:42 +0300

    Update profiles.js

--- profiles.js
+++ profiles.js
@@ -1,1 +1,26 @@
-export const profiles=[{id:1,name:'Ahmed'},];
+window.PROFILES = [
+    {
+        id: 1,
+        name: "احمد عبدالله",
+        age: 78,
+        education: "ابتدائي",
+        lastLogin: "2023-05-19",
+        risk: "high"
+    },
+    {
+        id: 2,
+        name: "فيصل احمد",
+        age: 27,
+        education: "جامعي",
+        lastLogin: "2025-08-18",
+        risk: "low"
+    },
+    {
+        id: 3,
+        name: "محمد سالم",
+        age: 42,
+        education: "ثانوي",
+        lastLogin: "2025-11-23",
+        risk: "medium"
+    }
+];
